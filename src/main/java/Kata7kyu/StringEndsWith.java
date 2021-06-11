f9aa1a09132605f35fa248ebd73b4079e0562a68
Abdi <abdi_665@hotmail.com>
2021-06-11 15:40:05 +0200

    Created class for the kata

---
+++ src/main/java/Kata7kyu/StringEndsWith.java
@@ -0,0 +1,4 @@
+package Kata7kyu;
+
+public class StringEndsWith {
+}
---
+++ src/test/java/Kata7kyu/StringEndsWith.java
@@ -0,0 +1,10 @@
+package Kata7kyu;
+
+import org.junit.jupiter.api.Test;
+
+public class StringEndsWith {
+    @Test
+    public void testSomething() {
+        // assertEquals("expected", "actual");
+    }
+}
